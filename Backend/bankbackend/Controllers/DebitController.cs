1dc931fac09f16da78fabeb03295f6ac22e0d76d
Marcel <sandermarcel1@t-online.de>
2022-06-27 10:12:40 +0200

    unnötige sachen Wieder gelöscht

--- Backend/bankbackend/Controllers/DebitController.cs
+++ Backend/bankbackend/Controllers/DebitController.cs
@@ -64,7 +64,7 @@ namespace bankbackend.Controllers
             string cookieValueFromReq = Request.Cookies["JWT"];
             Console.WriteLine(cookieValueFromReq);
             string a = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTE2MjM5MDIyfQ.MTek18-U2FKiOJvH89WskFJ9W-Yj8dK4zPgfkA-di2Q";
-            string b = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTk2MjM5MDIyfQ.Aqbekm4AhhNLhZC3eUwcZFOhFK_dX9l-ehYXZf3xcpI"
+            string b = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTk2MjM5MDIyfQ.Aqbekm4AhhNLhZC3eUwcZFOhFK_dX9l-ehYXZf3xcpI";
             JWTdecode jwtDecode = new JWTdecode();
             int id = jwtDecode.GetID(a);
            
@@ -99,13 +99,47 @@ namespace bankbackend.Controllers
         [Route("creat")]
         [HttpPost]
         // GET: api/Debit/creat
+        public string createinzahlung([FromBody] JsonElement body)
+        {
+
+            string cookieValueFromReq = Request.Cookies["JWT"];
+            Console.WriteLine(cookieValueFromReq);
+            string a = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTE2MjM5MDIyfQ.MTek18-U2FKiOJvH89WskFJ9W-Yj8dK4zPgfkA-di2Q";
+            string b = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTk2MjM5MDIyfQ.Aqbekm4AhhNLhZC3eUwcZFOhFK_dX9l-ehYXZf3xcpI";
+            JWTdecode jwtDecode = new JWTdecode();
+            int idsend = jwtDecode.GetID(a);
+            try
+            {
+                float balance = float.Parse(body.GetProperty("betrag").ToString());
+                float balancesend = balance * (-1);
+                int id = int.Parse(body.GetProperty("kontonr").ToString());
+                string reason = body.GetProperty("reason").ToString();
+                string query = "INSERT INTO public.debit(debitid, date, bankbalance, reason, customerid, fromuser) VALUES(Default,'" + DateTime.Now + "'," + balance + ", '" + reason + "', " + id + ", " + idsend + ");";
+                string sqlDatasource = _configuration.GetConnectionString("bankappcon");
+                using var con = new NpgsqlConnection(sqlDatasource);
+                con.Open();
+                using var cmd = new NpgsqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                return "funtkioniert";
+            }
+            catch (Exception ex)
+            {
+                return "fehler";
+            }
+
+
+            [Route("creat")]
+        [HttpPost]
+        // GET: api/Debit/creat
         public string creatdebiIndex([FromBody] JsonElement body)
         {
 
             string cookieValueFromReq = Request.Cookies["JWT"];
             Console.WriteLine(cookieValueFromReq);
             string a = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTE2MjM5MDIyfQ.MTek18-U2FKiOJvH89WskFJ9W-Yj8dK4zPgfkA-di2Q";
-            string b = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTk2MjM5MDIyfQ.Aqbekm4AhhNLhZC3eUwcZFOhFK_dX9l-ehYXZf3xcpI"
+                string b = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxOTk2MjM5MDIyfQ.Aqbekm4AhhNLhZC3eUwcZFOhFK_dX9l-ehYXZf3xcpI";
             JWTdecode jwtDecode = new JWTdecode();
             int idsend = jwtDecode.GetID(a);
             try
